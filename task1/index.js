2af44095a9e99994e8aed49f3496ffa3365e7b2c
Likhitha Maadhu <84303726+likhithamaadhu@users.noreply.github.com>
2021-09-21 13:15:55 +0530

    Add files via upload

---
+++ task1/index.js
@@ -0,0 +1,11 @@
+// function initMap(){
+
+//     var location ={lat: 30.3753, lng: 69.3451}
+    
+
+//     var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: location});
+
+//     var marker = new google.maps.Marker({
+//         position: location, map: map
+//     })
+// }
